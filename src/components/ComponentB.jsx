8d593a5365f7c1f8d94ffbca8204eb75a142c820
vishalpwaman <vishalpwaman1997@gmail.com>
2024-10-29 16:46:55 +0530

    Added : How to reinitialize data to formik

--- src/components/ComponentB.jsx
+++ src/components/ComponentB.jsx
@@ -2,6 +2,27 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
+const _staticData = [
+  {
+    firstName: "fname_example1",
+    lastName: "lname_example1",
+    age: 1,
+    comments: "comments_example1",
+  },
+  {
+    firstName: "fname_example2",
+    lastName: "lname_example2",
+    age: 2,
+    comments: "comments_example2",
+  },
+  {
+    firstName: "fname_example3",
+    lastName: "lname_example3",
+    age: 3,
+    comments: "comments_example3",
+  },
+];
+
 const initialValues = {
   firstName: "",
   lastName: "",
@@ -21,15 +42,33 @@ const validationSchema = Yup.object({
 });
 
 function ComponentB() {
+  const [newValues, setNewValues] = React.useState({
+    firstName: "",
+    lastName: "",
+    age: 0,
+    comments: "",
+  });
   console.log("Component B Render");
+
+  const handleEdit = (data) => {
+    setNewValues((prevState) => ({
+      ...prevState,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      age: data.age,
+      comments: data.comments,
+    }));
+  };
 
   return (
     <div className="container-fluid d-flex justify-content-center">
       <div className="w-50 h-50 bg-light p-5 mt-5">
         <Formik
-          initialValues={initialValues}
+          initialValues={newValues || initialValues}
           validationSchema={validationSchema}
           onSubmit={onSubmit}
+          validateOnMount
+          enableReinitialize
         >
           {(formik) => {
             console.log("Parent Formik : ", formik);
@@ -79,13 +118,55 @@ function ComponentB() {
                   />
                   <ErrorMessage name="age" component="span" className="error" />
                 </div>
-                <button className="btn btn-primary" type="submit">
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  disabled={!formik.isValid}
+                >
                   Submit
                 </button>
               </Form>
             );
           }}
         </Formik>
+        <div className="mt-3">
+          <table className="table">
+            <thead className="table-dark">
+              <tr>
+                <th>FirstName</th>
+                <th>LastName</th>
+                <th>Age</th>
+                <th>comment</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {_staticData.length > 0
+                ? _staticData.map((data, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{data.firstName}</td>
+                        <td>{data.lastName}</td>
+                        <td>{data.age}</td>
+                        <td>{data.comments}</td>
+                        <td>
+                          <button
+                            type="button"
+                            className="btn btn-link text-decoration-none"
+                            onClick={() => {
+                              handleEdit(data);
+                            }}
+                          >
+                            Edit
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })
+                : null}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
